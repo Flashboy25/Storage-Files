76bdb7444093e06de2842142e0745e39d7db8014
Promise obodoeze <126541418+Flashboy25@users.noreply.github.com>
2024-12-19 21:44:25 +0100

    Update playzbeta-post-ads.js

--- playzbeta-post-ads.js
+++ playzbeta-post-ads.js
@@ -2,14 +2,14 @@ function placeAds() {
   var wordCount = document.getElementById('post-body').innerText.split(' ').length;
 
   if (wordCount >= 140 && wordCount <= 160) {
-    document.getElementById('ad-1').innerHTML = '<p><script async="async" data-cfasync="false" src="//housecarefullyidiotic.com/90716a1bf53153829430440a519b0c90/invoke.js"></script>
-<div id="container-90716a1bf53153829430440a519b0c90"></div></p>';
+    document.getElementById('ad-1').innerHTML = '<p>< Ad 1 place </p>';
   }
   if (wordCount >= 300 && wordCount <= 350) {
     document.getElementById('ad-2').innerHTML = '<p>Ad 2 Placeholder</p>';
   }
   if (wordCount >= 500 && wordCount <= 600) {
-    document.getElementById('ad-3').innerHTML = '<p>Ad 3 Placeholder</p>';
+    document.getElementById('ad-3').innerHTML = '<p><script async="async" data-cfasync="false" src="//housecarefullyidiotic.com/90716a1bf53153829430440a519b0c90/invoke.js"></script>
+<div id="container-90716a1bf53153829430440a519b0c90"></div></p>';
   }
   if (wordCount >= 700 && wordCount <= 900) {
     document.getElementById('ad-4').innerHTML = '<p>Ad 4 Placeholder</p>';
